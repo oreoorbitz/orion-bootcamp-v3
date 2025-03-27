be8dc47ec0dad5c77fc117bd88c1376120b836d2
Adrian <total_play2016@hotmail.com>
2025-03-27 12:09:19 -0600

    ejercicio lua 1 completado

--- Ejercicios_etapa_1/Lua/Ejecicio_3/dobleMasTriple.lua
+++ Ejercicios_etapa_1/Lua/Ejecicio_3/dobleMasTriple.lua
@@ -16,17 +16,17 @@
 
 -- Función que devuelve el doble de un número
 local function doble(numero)
-    return 0 -- Reemplazar con la implementación correcta
+    return numero * 2 -- Reemplazar con la implementación correcta
 end
 
 -- Función que devuelve el triple de un número
 local function triple(numero)
-    return 0 -- Reemplazar con la implementación correcta
+    return numero * 3 -- Reemplazar con la implementación correcta
 end
 
 -- Función que devuelve la suma del doble y el triple de un número
 local function dobleMasTriple(numero)
-    return 0 -- Reemplazar con la implementación correcta
+    return doble(numero) + triple(numero) -- Reemplazar con la implementación correcta
 end
 
 -- Exportamos las funciones para que puedan ser requeridas en la prueba
