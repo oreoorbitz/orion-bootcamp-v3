335590360cb967a8fdcd1c3e8bec308655ba8425
Paola <soleystyles@gmail.com>
2025-04-11 17:12:55 -0600

    ejercicio 6 rust sin revisar

--- Ejercicios_etapa_1/Rust/vec/src/main.rs
+++ Ejercicios_etapa_1/Rust/vec/src/main.rs
@@ -11,28 +11,49 @@
 /// Si el índice es inválido, retorna None.
 fn acceder_array<T: Clone>(arr: &Vec<T>, index: usize) -> Option<T> {
     // TODO: Retornar el elemento en la posición indicada.
-    unimplemented!()
+    arr.get(index).cloned()
 }
 
 /// Manipula un array creando un nuevo vector y agregando tres elementos.
 /// Retorna el elemento en la posición 1 (segundo elemento) del vector.
 fn manipula_array<T: Clone>(primer_elemento: T, segundo_elemento: T, tercer_elemento: T) -> T {
     // TODO: Crea un vector vacío, agrega tres elementos y retorna el segundo elemento.
-    unimplemented!()
+  let mut vec_nuevo: Vec<T> = Vec::new();
+
+  vec_nuevo.push(primer_elemento);
+  vec_nuevo.push(segundo_elemento);
+  vec_nuevo.push(tercer_elemento);
+
+  vec_nuevo[1].clone()
+
 }
 
 /// Suma los elementos de un array de números usando un bucle for.
 /// Si el vector es homogéneo (todos números), retorna la suma total.
 fn sumar_elementos(numeros: &Vec<i32>) -> Result<i32, &'static str> {
     // TODO: Usar un bucle for para sumar los elementos del vector.
-    unimplemented!()
+    let mut suma = 0;
+
+    for &numero in numeros.iter() {
+      suma = suma + numero
+    }
+
+    Ok(suma)
 }
 
 /// Genera un vector con números desde 0 hasta n (incluyendo n).
 /// Retorna un error si n es negativo.
 fn generar_rango(n: i32) -> Result<Vec<i32>, &'static str> {
     // TODO: Verifica que n sea no negativo; luego, genera y retorna el vector.
-    unimplemented!()
+    if n < 0 {
+      return Err("Error n no puede ser negativo");
+    }
+
+    let mut rango: Vec<i32> = Vec::new();
+    for i in 0..=n {
+      rango.push(i);
+    }
+    Ok(rango)
 }
 
 /// Enum para representar valores heterogéneos: números o textos.
@@ -52,7 +73,16 @@ struct Organizado {
 /// Toma un vector de Valor y devuelve una estructura con dos campos: 'numbers' y 'strings'.
 fn organizar_por_tipo(arr: &Vec<Valor>) -> Organizado {
     // TODO: Recorrer el vector y clasificar cada elemento en el campo correspondiente.
-    unimplemented!()
+    let mut numbers = Vec::new();
+    let mut strings = Vec::new();
+
+    for valor in arr {
+      match valor {
+        Valor::Numero(n) => numbers.push(*n),
+        Valor::Texto(s) => strings.push(s.clone()),
+      }
+    }
+    Organizado { numbers, strings }
 }
 
 /// mi_mapa
@@ -64,7 +94,12 @@ where
     F: FnMut(&T) -> U,
 {
     // TODO: Implementar iterando con un bucle for y retornando el nuevo vector.
-    unimplemented!()
+    let mut resultado = Vec::new();
+
+    for i in arreglo {
+      resultado.push(fnc(i));
+    }
+    resultado
 }
 
 /// mi_filter
@@ -76,7 +111,14 @@ where
     F: FnMut(&T) -> bool,
 {
     // TODO: Implementar iterando con un bucle for y retornando el vector filtrado.
-    unimplemented!()
+    let mut filtrados = Vec::new();
+
+    for i in arreglo {
+      if predicado(i) {
+        filtrados.push(i.clone());
+      }
+    }
+    filtrados
 }
 
 /// mi_reduce
@@ -88,7 +130,12 @@ where
     F: FnMut(U, &T) -> U,
 {
     // TODO: Implementar usando un bucle for para acumular el resultado.
-    unimplemented!()
+    let mut acumulador = valor_inicial;
+
+    for i in arreglo {
+      acumulador = fnc(acumulador, i)
+    }
+    acumulador
 }
 
 /// usa_map
@@ -100,31 +147,49 @@ where
     F: FnMut(T) -> U,
 {
     // TODO: Implementar usando el método map de iteradores.
-    unimplemented!()
+    let iterador = arreglo.into_iter();
+
+    let iterador_transformado = iterador.map(fnc);
+
+    let resultado: Vec<U> = iterador_transformado.collect();
+
+    resultado
 }
 
 /// usa_filter
 ///
 /// Esta función utiliza el método de iteradores para simular el método .filter de JavaScript.
 /// Retorna un nuevo vector con los elementos que cumplen la condición especificada.
-fn usa_filter<T, F>(arreglo: Vec<T>, fnc: F) -> Vec<T>
+fn usa_filter<T, F>(arreglo: Vec<T>, mut fnc: F) -> Vec<T>
 where
     F: FnMut(&T) -> bool,
 {
     // TODO: Implementar usando el método filter de iteradores.
-    unimplemented!()
+    let iterador = arreglo.into_iter();
+
+    let iterador_filtrado = iterador.filter(|elem| fnc(elem));
+
+    let resultado = iterador_filtrado.collect();
+
+    resultado
 }
 
 /// usa_reduce
 ///
 /// Esta función utiliza el método fold para simular el método .reduce de JavaScript.
 /// Acumula un resultado a partir de los elementos del vector y retorna el resultado final.
-fn usa_reduce<T, U, F>(arreglo: Vec<T>, fnc: F, valor_inicial: U) -> U
+fn usa_reduce<T, U, F>(arreglo: Vec<T>, mut fnc: F, valor_inicial: U) -> U
 where
     F: FnMut(U, T) -> U,
 {
     // TODO: Implementar usando el método fold.
-    unimplemented!()
+    let iterador = arreglo.into_iter();
+
+    let resultado = iterador.fold(valor_inicial, |acumulador, elemento|{
+      fnc(acumulador, elemento)
+     });
+
+    resultado
 }
 
 fn main() {
@@ -132,7 +197,7 @@ fn main() {
     let arr = vec!['a', 'b', 'c'];
     let resultado_acceder = acceder_array(&arr, 2);
     println!("acceder_array: {:?}", resultado_acceder.unwrap()); // Espera 'c'
-    
+
     // Prueba de manipula_array:
     let resultado_manipular = manipula_array(1, 2, 3);
     println!("manipula_array: {}", resultado_manipular); // Espera 2
@@ -146,7 +211,7 @@ fn main() {
         Ok(rango) => println!("generar_rango: {:?}", rango),
         Err(err) => println!("generar_rango: {}", err),
     }
-    
+
     // Prueba de organizar_por_tipo:
     let entrada = vec![
         Valor::Numero(1),
@@ -156,27 +221,27 @@ fn main() {
     ];
     let organizado = organizar_por_tipo(&entrada);
     println!("organizar_por_tipo: {:?}", organizado);
-    
+
     // Prueba de mi_mapa:
     let resultado_mapa = mi_mapa(&vec![1, 2, 3, 4], |x| x * 2);
     println!("mi_mapa: {:?}", resultado_mapa); // Espera [2, 4, 6, 8]
-    
+
     // Prueba de mi_filter:
     let resultado_filter = mi_filter(&vec![1, 2, 3, 4, 5, 6], |x| x % 2 == 0);
     println!("mi_filter: {:?}", resultado_filter); // Espera [2, 4, 6]
-    
+
     // Prueba de mi_reduce:
     let resultado_reduce = mi_reduce(&vec![1, 2, 3, 4], |acc, x| acc + x, 0);
     println!("mi_reduce: {}", resultado_reduce); // Espera 10
-    
+
     // Prueba de usa_map:
     let resultado_usa_map = usa_map(vec![1, 2, 3, 4], |x| x * 3);
     println!("usa_map: {:?}", resultado_usa_map); // Espera [3, 6, 9, 12]
-    
+
     // Prueba de usa_filter:
     let resultado_usa_filter = usa_filter(vec![1, 2, 3, 4, 5, 6], |x| x % 2 != 0);
     println!("usa_filter: {:?}", resultado_usa_filter); // Espera [1, 3, 5]
-    
+
     // Prueba de usa_reduce:
     let resultado_usa_reduce = usa_reduce(vec![1, 2, 3, 4], |acc, x| acc * x, 1);
     println!("usa_reduce: {}", resultado_usa_reduce); // Espera 24
