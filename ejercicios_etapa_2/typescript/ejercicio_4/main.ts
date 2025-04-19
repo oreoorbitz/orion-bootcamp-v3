1cb8ec5475a3b5598ff6b7d85c0dfd73bdf166ca
orion holmes <oreooritz@gmail.com>
2025-04-18 19:48:33 -0600

    Wip

--- ejercicios_etapa_2/typescript/ejercicio_4/main.ts
+++ ejercicios_etapa_2/typescript/ejercicio_4/main.ts
@@ -45,3 +45,105 @@
  *
  * Conceptos clave: pila, estructuras anidadas, recorrido incremental
  */
+
+
+export enum TipoToken {
+  Apertura = 'apertura',
+  Cierre = 'cierre',
+  Autocierre = 'autocierre',
+  Texto = 'texto'
+}
+
+export interface Token {
+  nombre: string | null;
+  tipo: TipoToken;
+  contenido: string | null;
+  atributos: Record<string, string>
+  hijos: Token[]
+}
+
+export const objetizaratributo = (tag: string): Record<string, string> => {
+  const regex = /(\w+)="([^"]*)"/g;
+  const matches = [...tag.matchAll(regex)];
+  const result: Record<string, string> = {};
+  for (const match of matches) {
+    const key = match[1];
+    const value = match[2];
+    result[key] = value;
+  }
+  return result;
+}
+
+export const objetizarToken = (nombre: string | null, tipo: TipoToken, contenido: string | null, atributos: Record<string, string> ): Token => ({ nombre, tipo, contenido, atributos });
+
+const esText = (string: string) => /^[a-zA-Z]+$/.test(string.trim());
+
+const esTag = (string: string) => string.startsWith("<") && string.endsWith(">");
+
+const esAppertura = (string: string) => string.startsWith("<") && !string.startsWith("</") && !string.endsWith("/>");
+
+const esCierre = (string: string) => string.startsWith("</") && string.endsWith(">");
+
+const esAutocierre = (string: string) => string.startsWith("<") && string.endsWith("/>");
+
+const encontrarNombreDeTag = (string: string) => {
+  const regex = /<(\w+)/;
+  const match = string.match(regex);
+  return match ? match[1] : null;
+}
+
+const HTML = `<div class="greeting" >Hello <span>World</span></div>`;
+
+const tokenizarHTML = (html: string): string[] => {
+  const tokens = html.match(/<[^>]+>|[^<]+/g);
+  const tokenArray = tokens ? tokens : [];
+  return tokenArray
+}
+
+
+export const clasificarTokens = (tokens: string[]): object[] => {
+  const tokensClassificados = tokens.map(token => {
+    if (esTag(token)) {
+      const nombreTag = encontrarNombreDeTag(token);
+      if(esAppertura(token)) {
+        const atributos = objetizaratributo(token);
+        return objetizarToken(nombreTag, TipoToken.Apertura, null, atributos)
+      }
+      if(esCierre(token)) {
+        return objetizarToken(nombreTag, TipoToken.Cierre, null, {})
+      }
+      if(esAutocierre(token)) {
+        return objetizarToken(nombreTag, TipoToken.Autocierre,  null, {})
+      }
+      return null
+    }
+
+    if (esText(token)) {
+      return objetizarToken(null, TipoToken.Texto, token, {})
+    }
+
+    return null
+  })
+    .filter(token => token !== null)
+
+  return tokensClassificados
+}
+
+const construirArbol = (tokens: Token[]): Token | null => {
+  const obj = tokens.map(token => {
+    console.log(token.tipo === TipoToken.Apertura)
+    if(token.tipo === TipoToken.Apertura) {
+      return {
+        ...token,
+        hijos: []
+      }
+    }
+  })
+
+  return null
+}
+
+const tokens = tokenizarHTML(HTML);
+const tokensClasificados = clasificarTokens(tokens);
+construirArbol(tokensClasificados);
+
--- ejercicios_etapa_2/typescript/ejercicio_4/main_test.ts
+++ ejercicios_etapa_2/typescript/ejercicio_4/main_test.ts
@@ -1,1 +1,19 @@
 import { assertEquals } from "@std/assert";
+import { TipoToken, objetizaratributo, objetizarToken  } from "./main.ts"
+
+Deno.test("Test de la función objetizaratributo", () => {
+    const input = 'class="btn" id="main"';
+    const expectedOutput = { class: "btn", id: "main" };
+    const result = objetizaratributo(input);
+    assertEquals(result, expectedOutput);
+})
+
+Deno.test("Test de objetizarToken", () => {
+    const nombre = "div";
+    const tipo = TipoToken.Apertura;
+    const contenido = null;
+    const atributos = { class: "btn", id: "main" };
+    const expectedOutput = { nombre, tipo, contenido, atributos };
+    const result = objetizarToken(nombre, tipo, contenido, atributos);
+    assertEquals(result, expectedOutput);
+})
