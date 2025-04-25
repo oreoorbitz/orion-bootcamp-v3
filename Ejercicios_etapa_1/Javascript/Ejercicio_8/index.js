77673650d836903ab4a139e28c4563e5fc757095
orion holmes <oreooritz@gmail.com>
2025-04-25 15:25:51 -0600

    Fix errors in instructions

--- Ejercicios_etapa_1/Javascript/Ejercicio_8/index.js
+++ Ejercicios_etapa_1/Javascript/Ejercicio_8/index.js
@@ -1,6 +1,6 @@
 // Este archivo toma nuestros modulos, data, y utilidades y los une para cumplir la funcionalidad intencionada de este proyecto. Los imports en este archivo estan faltando. En algunos de los archivos, faltan les faltan exports.
 
-// importa el data de productos de data.js
+// importa el objecto products de product.js
 // importa getProductHandlesAndIds
 
 const resultado = getProductHandlesAndIds(products)
