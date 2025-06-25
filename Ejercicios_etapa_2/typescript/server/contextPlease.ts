ef64ad8471bf7f10e4d7e49083967e13343ff640
Paola <soleystyles@gmail.com>
2025-06-25 16:59:27 -0600

    holi

--- Ejercicios_etapa_2/typescript/server/contextPlease.ts
+++ Ejercicios_etapa_2/typescript/server/contextPlease.ts
@@ -1,49 +1,33 @@
+/*const coleccionesprueba=db.prepare('SELECT * FROM product_collections FULL JOIN products ON product_collections.productId=products.id FULL JOIN collections ON product_collections.collectionId=collections.id').all();
+console.log(coleccionesprueba);*/
 import { DatabaseSync } from "node:sqlite";
 const db = new DatabaseSync("data.db");
 
-const productos= db.prepare('SELECT * FROM products ORDER BY id').all();
-const colecciones= db.prepare('SELECT * FROM collections ORDER BY id').all();
+const products= db.prepare('SELECT * FROM products ORDER BY id').all();
+const collections = db.prepare('SELECT * FROM collections ORDER BY id').all();
 const coleccionesProductos=db.prepare('SELECT * FROM product_collections ORDER BY productId').all();
-console.log(typeof productos, productos);
-console.log(typeof colecciones, colecciones);
-console.log (typeof coleccionesProductos, coleccionesProductos);
 
-/*const organizar = arr =>{
-  let suave = [];
-  let promociones =[];
-  //Recorremos
-  for (let i= 0; i <arr.length ; i++ ) {
-    let contenido = arr[i];
-    if (typeof(contenido) === 'string') {
-      strings.push(contenido)
-    } else if (typeof(contenido) === 'number') {
-      numbers.push(contenido)
-    }
+const collectionsUnidas = collections.map( collection => {
+  //console.log(collection)
+  const productosCollectionFiltrado = coleccionesProductos.filter( cp => collection.id === cp.collectionId )
+  //console.log(productosDlaColeccion)
+  const productoJuntos = productosCollectionFiltrado.map( pc => products.find(producto => producto.id === pc.productId ))
+  console.log(productoJuntos)
+
+
+  return {
+    ...collection,
+    products: productoJuntos
   }
-}
+} )
+console.log(collectionsUnidas)
 
-/* const organizarPorTipo = (arr) => {
-    // TODO: Declara dos arrays, uno para strings y otro para numbers.
-  let strings = [];
-  let numbers = [];
-    // TODO: Recorre el array de entrada y clasifica cada elemento en el array correspondiente.
-  for (let i= 0; i <arr.length ; i++ ) {
-    let contenido = arr[i];
-    if (typeof(contenido) === 'string') {
-      strings.push(contenido)
-    } else if (typeof(contenido) === 'number') {
-      numbers.push(contenido)
-    }
-  }
-    // TODO: Retorna un objeto con las propiedades 'strings' y 'numbers' conteniendo los arrays correspondientes.
-    return {strings, numbers}; // Reemplazar por la implementación correcta.
-};
 
-/* export const context = {
+/*export const context = {
 settings:{
   titulo: "Rosita"
 },
 products,
-collections,
-productCollections
-}; */
+//collections,
+//productCollections
+};*/
